005963240438cb62882062f092ec45c0973baf23
felipe <profissionalf.andre@gmail.com>
2024-09-03 02:41:27 -0300

    chore: Update UI layout and responsive design for CodeAnywhere component

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -3,55 +3,57 @@ import Button from "./button";
 export default function Banner() {
   return (
     <>
-      <main className="bg-black opacity-95 text-white pb-10 border-b-gray-400 border-b-1">
-        <div className="flex justify-center">
-          <span className="border-2 border-solid rounded-full px-3 py-2 text-version_text border-slate-600 text-sm">
-            <b>Free. Built on open source. Runs everwhere.</b>
-          </span>
-        </div>
-        <section className="flex items-center flex-wrap py-4 flex-col ">
-          <h1 className="text-5xl text-center leading-tight">
-            <strong>Code Editing. Redefined.</strong>
-          </h1>
-          <div className="flex gap-1 py-3">
-            <Button>
-              <div className="flex justify-center">
-                <img
-                  src="https://code.visualstudio.com/assets/icons/download.svg"
-                  alt="Download"
-                  className="w-5"
-                />
-                <span>
-                  <b>
-                    &nbsp;.<abbr title="Debian">deb</abbr>
-                  </b>
-                </span>
-              </div>
-              <small className="font-medium">Debian, Ubuntu...</small>
-            </Button>
-            <Button>
-              <div className="flex justify-center">
-                <img
-                  src="https://code.visualstudio.com/assets/icons/download.svg"
-                  alt="Download"
-                  className="w-5"
-                />
-                <span>
-                  <b>&nbsp;.rpm</b>
-                </span>
-              </div>
-              <small className="font-medium">Red Hat, Fedora...</small>
-            </Button>
+      <main className="bg-black opacity-95 text-white pb-10 border-b-gray-400 border-b-1 md:grid md:grid-cols-[1fr_65%] md:gap-16 md:px-[6rem] md:pb-32">
+        <div className="xl:flex xl:justify-center xl:flex-col xl:items-start">
+          <div className="flex justify-center">
+            <span className="border-2 border-solid rounded-full px-3 py-2 text-version_text border-slate-600 text-sm">
+              <b>Free. Built on open source. Runs everwhere.</b>
+            </span>
           </div>
+          <section className="flex items-center flex-wrap py-4 flex-col xl:items-start">
+            <h1 className="text-5xl text-center leading-tight xl:text-start xl:text-6xl xl:leading-snug">
+              <strong>Code Editing. Redefined.</strong>
+            </h1>
+            <div className="flex gap-1 py-3">
+              <Button>
+                <div className="flex justify-center">
+                  <img
+                    src="https://code.visualstudio.com/assets/icons/download.svg"
+                    alt="Download"
+                    className="w-5"
+                  />
+                  <span>
+                    <b>
+                      &nbsp;.<abbr title="Debian">deb</abbr>
+                    </b>
+                  </span>
+                </div>
+                <small className="font-medium">Debian, Ubuntu...</small>
+              </Button>
+              <Button>
+                <div className="flex justify-center">
+                  <img
+                    src="https://code.visualstudio.com/assets/icons/download.svg"
+                    alt="Download"
+                    className="w-5"
+                  />
+                  <span>
+                    <b>&nbsp;.rpm</b>
+                  </span>
+                </div>
+                <small className="font-medium">Red Hat, Fedora...</small>
+              </Button>
+            </div>
 
-          <span className="text-sky-600 underline pb-3">
-            Web, insiders edition <span className="text-version_text">or</span>{" "}
-            other plataforms
-          </span>
-          <small className="text-version_text text-xs">
-            By using VS Code, you agree to its license and privacy statement.
-          </small>
-        </section>
+            <span className="text-sky-600 underline pb-3">
+              Web, insiders edition{" "}
+              <span className="text-version_text">or</span> other plataforms
+            </span>
+            <small className="text-version_text text-xs">
+              By using VS Code, you agree to its license and privacy statement.
+            </small>
+          </section>
+        </div>
 
         <div className="flex justify-center">
           <img
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,46 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [search, setSearch] = useState("");
+
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
+  }
+
   return (
-    <header className="p-3 bg-black opacity-95 h-13">
+    <header className="px-28 py-3 bg-black opacity-95 h-13 fixed top-0 left-0 z-50 w-full">
       <div className="flex gap-3 items-center">
         <img className="w-7" src="images/download.png" alt="Logotipo VSCode" />
-        <span className="text-white text-xl">
+        <span className="text-white text-lg mr-4">
           <b>Visual Studio Code</b>
         </span>
+        <ol className="text-version_text flex gap-5">
+          <li>Docs</li>
+          <li>Updates</li>
+          <li>Blog</li>
+          <li>API</li>
+          <li>Extensions</li>
+          <li>FAQ</li>
+        </ol>
+
+        <div className="flex flex-1 justify-end items-center">
+          <img
+            src="images/sun.svg"
+            alt="Ícone de busca"
+            className="w-4 h-4 mr-3"
+          />
+
+          <input
+            type="text"
+            onChange={handleSearch}
+            value={search}
+            placeholder="Search docs"
+            className="p-2 mr-3 text-white bg-background_cards_extensions border-none rounded-md"
+          />
+          <button className="bg-background_buttons w-28 h-9 text-white text-center rounded-md">
+            <strong>Download</strong>
+          </button>
+        </div>
       </div>
     </header>
   );
