4f6e9a4194dc1e0aed0540918cb458e51dfe56d6
felipe <profissionalf.andre@gmail.com>
2024-09-03 03:19:24 -0300

    fix: resolve vercel build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import Header from "./components/header";
 import Banner from "./components/banner";
 import CodeAnyLanguage from "./components/languages";
